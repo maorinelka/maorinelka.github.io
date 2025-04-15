eba66ab406764054f020ec8df652c5634a529118
maorinelka <89161876958@mail.ru>
2025-04-15 12:12:04 +0300

    Файлы сайта-визитки

---
+++ js.js
@@ -0,0 +1,10 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button1.addEventListener('click', function() {
+	alert('Время есть куличи!')
+
+		
+
+})
